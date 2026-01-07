090fcdcbe2c6caf94fc642ffefb265504a5701c6
LucasMedici <lucasrpmedici@gmail.com>
2026-01-07 20:16:41 -0300

    Initial commit files

---
+++ src/main/java/video/streaming/platform/streamly/StreamlyApplication.java
@@ -0,0 +1,13 @@
+package video.streaming.platform.streamly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/video/streaming/platform/streamly/StreamlyApplicationTests.java
@@ -0,0 +1,13 @@
+package video.streaming.platform.streamly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
