58313e55c3205100c30315053b782595d53cb00e
LucasMedici <lucasrpmedici@gmail.com>
2026-01-13 16:43:08 -0300

    feat: creating a passwordEnconder to create User

--- src/main/java/video/streaming/platform/streamly/user/UserService.java
+++ src/main/java/video/streaming/platform/streamly/user/UserService.java
@@ -1,22 +1,24 @@
 package video.streaming.platform.streamly.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(CreateUserDTO createUserDTO){
         var newUser = new User(createUserDTO.name(),
                             createUserDTO.email(),
-                            createUserDTO.password(),
+                            passwordEncoder.encode(createUserDTO.password()),
                             UserRoles.USER);
         return userRepository.save(newUser);
-
     }
 }
